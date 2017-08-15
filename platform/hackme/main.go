1e5b0838979a48df68bd5780b63c4237665dd3f2
andygeiss <andygeiss>
2017-08-15 15:49:11 +0200

    switched to endless loop

--- platform/hackme/main.go
+++ platform/hackme/main.go
@@ -10,14 +10,8 @@ var IsAdmin bool
 
 func main() {
 	IsAdmin = false
-	times := 0
-	// We have 10 seconds to hack IsAdmin ;-)
 	for {
 		time.Sleep(time.Second * 1)
 		fmt.Printf("Hackme [Admin: %v, Address: %p]\n", IsAdmin, &IsAdmin)
-		times++
-		if times > 10 {
-			break
-		}
 	}
 }
