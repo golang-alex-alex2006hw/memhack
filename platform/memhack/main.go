d23984296757be404e9ecc5020c75d3e7c3edd11
andygeiss <andygeiss>
2017-08-15 16:08:31 +0200

    reorganized

--- platform/memhack/main.go
+++ platform/memhack/main.go
@@ -9,56 +9,83 @@ import (
 )
 
 func main() {
-	// Prepare command execution
-	fmt.Printf("Running %v ...\n", os.Args[2:])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	//
+	addr := os.Args[1]
+	this := os.Args[2]
+	args := os.Args[3:]
+	//
+	cmd, err := runCommand(this, args)
+	//
+	ptr := getPointer(addr)
+	//
+	var regs syscall.PtraceRegs
+	pid := cmd.Process.Pid
+	exit := true
+	for {
+		if exit {
+			err = syscall.PtraceGetRegs(pid, &regs)
+			// no such process
+			if err != nil {
+				break
+			}
+		}
+		hackAdmin(pid, ptr)
+		nextSyscall(pid)
+		wait4Syscall(pid)
+		exit = !exit
+	}
+	fmt.Print("Exit.") // possibly never reached
+}
+
+// transform string address into pointer
+func getPointer(addr string) int64 {
+	dst, err := strconv.ParseInt(addr, 0, 64)
+	if err != nil {
+		panic(err)
+	}
+	return dst
+}
+
+// set bool value of IsAdmin to true
+func hackAdmin(pid int, addr int64) {
+	//buf := make([]byte, 1)
+	//_, err = syscall.PtracePeekData(pid, 0x59fac2, buf[:1])
+	_, err := syscall.PtracePokeData(pid, uintptr(addr), []byte{1})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// trace next syscall
+func nextSyscall(pid int) {
+	err := syscall.PtraceSyscall(pid, 0)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// prepare & run command
+func runCommand(this string, args []string) (*exec.Cmd, error) {
+	fmt.Printf("Running %v ...\n", this)
+	cmd := exec.Command(this, args...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Ptrace: true,
 	}
-	addr, err := strconv.ParseInt(os.Args[1], 0, 64)
-	if err != nil {
-		panic(err)
-	}
-	// Start command
 	cmd.Start()
-	err = cmd.Wait()
+	err := cmd.Wait()
 	if err != nil {
 		fmt.Printf("Wait returned: %v\n", err)
 	}
-	// Main loop
-	var regs syscall.PtraceRegs
-	pid := cmd.Process.Pid
-	exit := true
-	for {
-		// If error occured (no such process) then leave main loop
-		if exit {
-			err = syscall.PtraceGetRegs(pid, &regs)
-			if err != nil {
-				break
-			}
-		}
-		//buf := make([]byte, 1)
-		//_, err = syscall.PtracePeekData(pid, 0x59fac2, buf[:1])
+	return cmd, err
+}
 
-		// Set bool value of IsAdmin to true.
-		syscall.PtracePokeData(pid, uintptr(addr), []byte{1})
-		if err != nil {
-			panic(err)
-		}
-		// Trace next syscall
-		err = syscall.PtraceSyscall(pid, 0)
-		if err != nil {
-			panic(err)
-		}
-		// Wait for syscall exit
-		_, err = syscall.Wait4(pid, nil, 0, nil)
-		if err != nil {
-			panic(err)
-		}
-		exit = !exit
+// wait for syscall exit
+func wait4Syscall(pid int) {
+	_, err := syscall.Wait4(pid, nil, 0, nil)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Print("Exit.")
 }
